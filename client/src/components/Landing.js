cc32d4433277c00c1eac8e31b029475e0fa84384
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-09-30 17:08:53 -0700

    added eslint airbnb

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react';
 
 class Landing extends Component {
   render() {
     return (
-      <div>
+      <Container>
         Yay!
-      </div>
+      </Container>
     );
   }
 }
