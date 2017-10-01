0068130cead3fec33630a2ab47a2da4290b102bb
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-09-30 17:30:30 -0700

    created navbar

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { Container } from 'semantic-ui-react';
 
+import Navbar from './Navbar';
+
 class Landing extends Component {
   render() {
     return (
       <Container>
+        <Navbar />
         Yay!
       </Container>
     );
