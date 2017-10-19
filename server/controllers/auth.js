3ea434b2c41ef0871ed51aa7693f0627be33b9de
Rudy Wahjudi <rudy.wahjudi@gmail.com>
2017-10-19 16:26:20 -0700

    modified signup to insert to mongodb and mysql db

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,19 +1,97 @@
-const User = require('../models/user');
+const User = require('../models/mongoose_user');
 const passwordHash = require('password-hash');
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
 require('dotenv').config();
+const fs = require('fs');
+//mysql
+const db = require('../models');
 
 exports.signup = (req, res, next) => {
   const data = req.body;
   data.password = passwordHash.generate(req.body.password);
 
-  User.create( data, (err, user) => {
-    if (err) {
-      res.json(err);
+  User.findOne({ email: data.email }, (err, user) => {
+    if (user) {
+      const errorMsg = {
+        message: 'Email is already used. Please sign up using different email.',
+      };
+      res.json(errorMsg);
     } else {
-      res.json(user);
-    }
+
+      // mysql
+      db.User.create({
+        firstname: data.firstname,
+        lastname: data.lastname,
+        email: data.email,
+        password: data.password,
+        // id: newUser._id,
+      })
+        .then((mysqlUser) => {
+          console.log(`created a new user in mysql db`, mysqlUser);
+          // res.json(user);
+
+          // create uniq directory
+          var dir = `./public/uploads/${mysqlUser.email}`;
+          // create dir if it doesn't exist
+          if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+          }
+        
+          // need to create a new entry in the about and interest tables
+          const new_about = {
+            overview: '',
+            work_edu: '',
+            contact_info: '',
+            life_events: '',
+            user_id: mysqlUser.id,
+          };
+          db.About.create(new_about)
+            .then((about) => {
+              console.log('created a new about');
+
+              const new_interest = {
+                music: '',
+                shows: '',
+                sports: '',
+                fav_teams: '',
+                user_id: mysqlUser.id,
+              };
+              db.Interest.create(new_interest)
+                .then((interest) => {
+                  // console.log('create new interest', interest);
+
+                  User.create({
+                    firstname: mysqlUser.firstname,
+                    lastname: mysqlUser.lastname,
+                    email: mysqlUser.email,
+                    password: mysqlUser.password,
+                    mysql_id: mysqlUser.id,
+                  }, (err2, newUser) => {
+                    if (err2) {
+                      res.json(err2);
+                    } else {
+                      res.json(newUser);                          
+                    }
+                  });
+            
+                });
+            });
+        });
+
+      // res.json(newUser);
+
+    } // end of else
   });
+
+  // User.create(data, (err, user) => {
+  //   if (err) {
+  //     res.json(err);
+  //   } else {
+  //     res.json(user);
+  //   }
+  // });
 };
 
 exports.signin = (req, res) => {
